9efbe0bddb0eaf7d9913368233c0c2893c6f9c25
pkevlak <pauloosha@gmail.com>
2019-09-10 16:51:21 +0300

    Update configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,9 +5,7 @@ import rootReducer from '../reducers';
 const configureStore = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
